a7547e32a3d2ddae5182d52a29f2b06baf4a4c61
Jonas Johan solsvik <jonasjso@protonmail.com>
2019-10-24 00:38:47 +0200

    main.c cleanup

--- main.c
+++ main.c
@@ -6,35 +6,19 @@
 #include <signal.h>
 #include "./playland/playland.h"
 
-static bool click = false;
-static bool escape = false;
-
-volatile sig_atomic_t quit = 0;
-
-
 static struct playland* playland = NULL;
 
-/**
- * Handle SIGINT
- */
-void 
+volatile sig_atomic_t quit = 0;
+void
 sigHandler(int dummy) {
     perror("Quitting gracefully...\n");
     quit = 1;
-}
-
-static int status = EXIT_SUCCESS;
-#define GOTO(symbol) {\
-    status = EXIT_FAILURE;\
-    goto symbol;\
 }
 
 void
 on_button(uint32_t button) {
     // click = true;
 }
-
-
 
 void
 on_key(void* _playland, void* _window, uint32_t key) {
@@ -52,8 +36,8 @@ on_key(void* _playland, void* _window, uint32_t key) {
     }
     if (key == KeyF) {
         wl_shell_surface_set_fullscreen(
-            window->shell_surface, 
-            WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT, 
+            window->shell_surface,
+            WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT,
             120000,
             playland->output
         );
@@ -69,8 +53,17 @@ on_key(void* _playland, void* _window, uint32_t key) {
     }
 }
 
+#define GOTO(symbol) {\
+    status = EXIT_FAILURE;\
+    goto symbol;\
+}
+
 int
 main() {
+    int status = EXIT_SUCCESS;
+    //
+    // Init
+    //
     signal(SIGINT, sigHandler);
     signal(SIGQUIT, sigHandler);
     signal(SIGTERM, sigHandler);
@@ -79,38 +72,28 @@ main() {
     if (! playland) {
         GOTO(panic);
     }
-    //
-    // 0. Create resource file and corresponding bufferes
-    //
     struct playland_file* const images = playland_create_file(playland, "images.bin");
     if (! images) {
         GOTO(panic_playland);
     }
-
     struct wl_buffer* const window_background = playland_file_create_buffer(images, 320, 200);
     if (! window_background) {
         GOTO(panic_images);
     }
-
     struct wl_buffer* const cursor_sprite = playland_file_create_buffer(images, 100, 59);
     if (! cursor_sprite) {
         GOTO(panic_window_background);
     }
-    //
-    // 1. Create window
-    //
     struct playland_window* const  window = playland_create_window(playland, "Main window");
     if (! window) {
         GOTO(panic_cursor_sprite);
     }
     playland_window_set_background(window, window_background);
-    //
-    // 2. Create cursor
-    //
+
     struct playland_cursor* const cursor = playland_create_cursor(playland);
     if (! cursor) {
         GOTO(panic_window);
-    }   
+    }
     cursor->on_button = on_button;
     playland_cursor_set_sprite(cursor, cursor_sprite, 35, 10);
 
@@ -120,16 +103,15 @@ main() {
     }
     keyboard->on_key = on_key;
     keyboard->on_key_payload = window;
-
     //
-    // 3. Listen for events
+    // Loop
     //
     bool done = false;
     while (! done && ! quit) {
         done = playland_listen(playland);
     }
     //
-    // 4. Cleanup
+    // Cleanup
     //
     free(keyboard);
 panic_cursor:
@@ -145,8 +127,5 @@ panic_images:
 panic_playland:
     playland_destroy(playland);
 panic:
-    //
-    // 5. Exit
-    //
     return status;
 }
